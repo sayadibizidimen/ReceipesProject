dbe0aec071e82241e2edf685c74e9d91ccdae06e
imen <sayadibizidimen@gmail.com>
2023-03-14 13:00:16 +0100

    feat: init the project

---
+++ ReceipesProject/ReceipesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipesProjectApp.swift
+//  ReceipesProject
+//
+//  Created by sayadi imen on 14/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReceipesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

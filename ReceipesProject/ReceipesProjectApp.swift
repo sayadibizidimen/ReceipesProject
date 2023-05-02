0f862f703d213cb328fe2dd225ea13f278fd2a49
imen <sayadibizidimen@gmail.com>
2023-05-02 19:41:10 +0100

    feat:configuration du firebase

--- ReceipesProject/ReceipesProjectApp.swift
+++ ReceipesProject/ReceipesProjectApp.swift
@@ -17,11 +17,22 @@ struct ReceipesProjectApp: App {
 }
 */
 import SwiftUI
+import Firebase
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 @main
 struct ReceipesProjectApp: App {
+  
     @StateObject private var modelData = ModelData()
-
+   @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -29,3 +40,6 @@ struct ReceipesProjectApp: App {
         }
     }
 }
+
+
+
